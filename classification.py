1ba5794cc95f3085dc0abc443adec7f653649a0c
AdamStan <adam15stan@gmail.com>
2019-12-02 23:11:37 +0100

    F is calculated but in one dimension and not every value is calculated

---
+++ classification.py
@@ -0,0 +1,64 @@
+import numpy as np
+import math
+
+def wspolczynnik_F_in_one_(matrix1, matrix2):
+    """
+    returns matrix of F wspolczynnikow
+    """
+    pass
+
+def load_matrixes(path_to_file = "Maple_Oak.txt"):
+    matrixes = {}
+    with open(path_to_file) as file:
+        # skip first line
+        line = file.readline()
+        # read correct line
+        line = file.readline()
+        while line:
+            line = line.replace('\n', '').split(',')
+            name = line[0]
+            numbers = line[1:]
+            for i in range(len(numbers)):
+                numbers[i] = float(numbers[i])
+            if name in matrixes:
+                matrixes[name] = np.append(matrixes.get(name), [numbers], axis = 0)
+            else:
+                matrixes[name] = np.array([numbers])
+            line = file.readline()
+        file.close()
+
+    return matrixes
+
+def calculate_mean_vectors(matrixes):
+    means = []
+    for matrix in matrixes:
+        means.append(matrix.mean(1))
+    return means
+
+def calculate_standard_deviation_vetors(matrixes):
+    standard_deviations = []
+    for matrix in matrixes:
+        standard_deviations.append(matrix.std(1))
+    return standard_deviations
+
+def calculate_f_in_one_dimension(mean_vectors, std_vectors):
+    f_results = {}
+    for i in range(len(mean_vectors) - 1):
+        for j in range(len(mean_vectors[0])):
+            numerator = math.fabs(mean_vectors[i].item(j) - mean_vectors[i+1].item(j))
+            denominator = std_vectors[i].item(j) + std_vectors[i+1].item(j)
+            f_results[str(i+1) + "_" + str(j+1)] = (numerator / denominator)
+    
+    return f_results
+
+def main():
+    matrixes = load_matrixes()
+    mean_vecotors = calculate_mean_vectors(matrixes.values())
+    std_vectors = calculate_standard_deviation_vetors(matrixes.values())
+    f_in_one_dimension = calculate_f_in_one_dimension(mean_vecotors, std_vectors)
+    print(mean_vecotors)
+    print(std_vectors)
+    print(f_in_one_dimension)
+
+if __name__ == "__main__":
+    main()
---
+++ test.py
@@ -0,0 +1,92 @@
+import numpy as np
+import math
+from classification import calculate_mean_vectors, calculate_standard_deviation_vetors, calculate_f_in_one_dimension
+import unittest
+
+class TestClassification(unittest.TestCase):
+    def setUp(self):
+        matrix_A = np.array([[3, 2, 2, 1], [2, 3, 0, 7], [-1, 2, 2, -3]])
+        matrix_B = np.array([[3, 1, 5], [6, 4, 5], [-1, 3, -2]])
+        
+        self.matrixes = [
+            matrix_A,
+            matrix_B
+        ]
+
+        self.means_v = [
+            matrix_A.mean(1),
+            matrix_B.mean(1)
+        ]
+
+        self.std_v = [
+            matrix_A.std(1),
+            matrix_B.std(1)
+        ]
+
+        self.f_in_one_dimension = [
+            (math.fabs(self.means_v[0].item(0) - self.means_v[1].item(0))) / (self.std_v [0].item(0) + self.std_v [1].item(0)),
+            (math.fabs(self.means_v[0].item(1) - self.means_v[1].item(1))) / (self.std_v [0].item(1) + self.std_v [1].item(1)),
+            (math.fabs(self.means_v[0].item(2) - self.means_v[1].item(2))) / (self.std_v [0].item(2) + self.std_v [1].item(2))
+        ]
+
+    def test_calculating_mean_vectors(self):
+        means = calculate_mean_vectors(self.matrixes)
+        for i in range(len(self.means_v)):
+            for j in range(len(self.means_v[i])):
+                self.assertEqual(self.means_v[i].item(j), means[i].item(j))
+
+    def test_calculating_std_vectors(self):
+        stds = calculate_standard_deviation_vetors(self.matrixes)
+        for i in range(len(self.std_v)):
+            for j in range(len(self.std_v[i])):
+                self.assertEqual(self.std_v[i].item(j), stds[i].item(j))
+
+    def test_calculating_f_vector(self):
+        f_results = calculate_f_in_one_dimension(self.means_v, self.std_v)
+        results = list(f_results.values())
+        for i in range(len(self.f_in_one_dimension)):
+            self.assertEquals(self.f_in_one_dimension[i], results[i])
+
+if __name__ == "__main__":
+    unittest.main()
+
+
+def test_with_example_of_lab1_in_one_dimension():
+    """
+    deprecated tests
+    """
+    matrix_A = np.array([[3, 2, 2, 1], [2, 3, 0, 7], [-1, 2, 2, -3]])
+    matrix_B = np.array([[3, 1, 5], [6, 4, 5], [-1, 3, -2]])
+    
+    matrixes = {
+        "Matrix A": matrix_A,
+        "Matrix B": matrix_B
+    }
+
+    means = calculate_mean_vectors(matrixes.values())
+
+    mean_A = matrix_A.mean(1)
+    print(mean_A)
+    mean_B = matrix_B.mean(1)
+    print(mean_B)
+    print(means)
+
+    stds = calculate_standard_deviation_vetors(matrixes.values())
+
+    standard_deviation_A = matrix_A.std(1)
+    print(standard_deviation_A)
+    standard_deviation_B = matrix_B.std(1)
+    print(standard_deviation_B)
+    print(stds)
+    
+    if(len(mean_A) != len(mean_B)):
+        raise Exception("Amount of rows in matrixes is not the same!")
+
+    f_results = []
+    for i in range(len(mean_A)):
+        numerator = math.fabs(mean_A[i] - mean_B[i])
+        denominator = standard_deviation_A[i] + standard_deviation_B[i]
+        f_results.append(numerator / denominator)
+    
+    print(f_results)
+    print(calculate_f_in_one_dimension(means, stds))
